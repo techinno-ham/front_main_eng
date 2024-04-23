ad5c64be271aff2989b56e8caf1cc49deba543c8
mahdiyar <mahdiyar@pro.ai>
2024-04-23 12:27:32 +0100

    first commit

---
+++ src/modules/landing/index.tsx
@@ -0,0 +1,14 @@
+
+
+
+const Landing=()=>{
+    return(
+        <>
+        <div>
+        <p className="text-3xl font-bold underline"> test</p>
+        </div>
+        </>
+    )
+};
+
+export default Landing;
