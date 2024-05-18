e8c87be16629507c4ae920a1a116bb1448cecf57
moein moeinnia <moein@pro.ai>
2024-05-18 13:49:06 +0330

    chore: pricing added

---
+++ app/pricing/page.tsx
@@ -0,0 +1,9 @@
+import MyBots from "@/src/modules/myBots"
+import Pricing from "@/src/modules/pricing"
+import { NextPage } from "next"
+
+const PricingPage: NextPage = () => {
+    return <Pricing/>
+}
+
+export default PricingPage
