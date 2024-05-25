ff21aba86579279135abc8dfdc8369b6269a2841
moein moeinnia <moein@pro.ai>
2024-05-25 15:25:41 +0330

    chore: slider added fo rabout us

--- src/shared/components/layouts/site-layout/footer/index.tsx
+++ src/shared/components/layouts/site-layout/footer/index.tsx
@@ -5,7 +5,7 @@ import Image from "next/image"
 const Footer = () => {
     return (
         <footer className="footer-main">
-            <div className="container mx-auto px-4" >
+            <div className="container mx-auto px-4">
                 <div className="wrapper-section">
                     <div className="grid-footer">
                         <div className="column w-node">
