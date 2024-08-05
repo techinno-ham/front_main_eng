83f26a035a33e40e3a8ad8acc4abf5134b65e999
mahdiyar <mahdiyar@pro.ai>
2024-08-05 10:59:45 +0330

    feat: add new sction for landing

--- src/modules/landing/components/introduceSections/index.tsx
+++ src/modules/landing/components/introduceSections/index.tsx
@@ -7,7 +7,7 @@ import { ClockLoader } from "react-spinners"
 import Services from "../../../../../src/shared/services/service"
 import LoaderLottie from "@/src/shared/components/common/loader"
 import { toast } from "sonner"
-import { Calendar } from "iconsax-react"
+import { Calendar, DocumentText1, DollarCircle, Messages1, Messages3, Timer1 } from "iconsax-react"
 
 const IntroduceSections = () => {
     const [inputValue, setInputValue] = useState("")
@@ -137,6 +137,46 @@ const IntroduceSections = () => {
                         alt="Logo"
                     />
                 </div>
+                <div className="grid gap-y-10 gap-x-10 md:grid-cols-2 lg:grid-cols-4 mt-16">
+                    <div className="relative flex flex-col bg-clip-border rounded-xl bg-transparent text-gray-700 shadow-none items-center">
+                         <Messages3
+                            size="28"
+                            color="#212121"
+                           />
+                           <h1 className="block antialiased tracking-normal font-sans mb-2 mt-4 text-5xl text-gray-900 font-bold">24/7</h1>
+                           <h5 className="block antialiased tracking-normal font-sans text-xl leading-snug mb-2 font-semibold text-gray-600">دستیار پشتیبانی</h5>
+                           <h6 className="block antialiased tracking-normal font-sans text-base leading-relaxed mb-2 font font-normal text-gray-600">در مقایسه با پشتیبانی استاندارد، چت بات 24/7 برای مشتریان شما در دسترس است.</h6>
+                    </div>
+                    <div className="relative flex flex-col bg-clip-border rounded-xl bg-transparent text-gray-700 shadow-none items-center">
+                          <DocumentText1
+                            size="28"
+                            color="#212121"
+                           />
+                           <h1 className="block antialiased tracking-normal font-sans mb-2 mt-4 text-5xl text-gray-900 font-bold">100%</h1>
+                           <h5 className="block antialiased tracking-normal font-sans text-xl leading-snug mb-2 font-semibold text-gray-600">دقت</h5>
+                           <h6 className="block antialiased tracking-normal font-sans text-base leading-relaxed mb-2 font font-normal text-gray-600">چت بات ما در پاسخ به سوالات دقت 100% دارد.</h6>
+                    </div>
+                    <div className="relative flex flex-col bg-clip-border rounded-xl bg-transparent text-gray-700 shadow-none items-center">
+                          <Timer1
+                            size="28"
+                            color="#212121"
+                           />
+                           <h1 className="block antialiased tracking-normal font-sans mb-2 mt-4 text-5xl text-gray-900 font-bold">3x</h1>
+                           <h5 className="block antialiased tracking-normal font-sans text-xl leading-snug mb-2 font-semibold text-gray-600">پاسخ سریعتر</h5>
+                           <h6 className="block antialiased tracking-normal font-sans text-base leading-relaxed mb-2 font font-normal text-gray-600"> چت بات ما 3 برابر سریعتر از یک انسان پاسخ می دهد.</h6>
+                    </div>
+                    <div className="relative flex flex-col bg-clip-border rounded-xl bg-transparent text-gray-700 shadow-none items-center">
+                          <DollarCircle
+                            size="28"
+                            color="#212121"
+                           />
+                           <h1 className="block antialiased tracking-normal font-sans mb-2 mt-4 text-5xl text-gray-900 font-bold">50%-</h1>
+                           <h5 className="block antialiased tracking-normal font-sans text-xl leading-snug mb-2 font-semibold text-gray-600">هزینه های پشتیبانی مشتری</h5>
+                           <h6 className="block antialiased tracking-normal font-sans text-base leading-relaxed mb-2 font font-normal text-gray-600"> تا 50 درصد از هزینه های پشتیبانی مشتری خود را به ازای هر نماینده کاهش دهید.</h6>
+                    </div>
+                    
+                </div>
+
             </div>
 
             {loading && (
