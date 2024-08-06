9883ff47e8d4bc8fa95d484e7385e84a1115bc9b
moein moeinnia <moein@pro.ai>
2024-08-06 15:19:52 +0330

    aff pg

---
+++ src/modules/Affiliate/index.tsx
@@ -0,0 +1,218 @@
+//https://www.galichat.com/partners
+
+const Affiliate = () => {
+    return (
+        <>
+            <div className="relative min-h-full w-full bg-[url('/header-bg.jpg')] bg-contain bg-no-repeat lg:bg-cover">
+                <div className="relative z-10 grid min-h-full px-8">
+                    <div className="container relative mx-auto my-auto pb-36 pt-40 text-center">
+                        <h1 className="text-blue-gray-900 block font-sans text-5xl font-extrabold leading-tight tracking-normal antialiased">
+                            Get Paid. 30% Per Sale <br />
+                            <span className="font-medium">
+                                Become our Partner Today.
+                            </span>
+                        </h1>
+                        <p className="mt-7 block font-sans text-xl font-medium leading-relaxed text-gray-600 antialiased md:px-32 lg:px-52">
+                            Recommend our AI website chatbot to your audience or
+                            clients and{" "}
+                            <span className="font-bold text-black">
+                                make extra money
+                            </span>
+                            .
+                        </p>
+                        <a
+                            href="https://affiliates.galichat.com/login"
+                            target="_blank"
+                            className="mt-8 block"
+                        >
+                            <button
+                                className="select-none rounded-lg bg-gray-900 px-6 py-3
+                         text-center align-middle font-sans text-xs font-bold 
+                         uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none
+                          active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                                type="button"
+                            >
+                                Join Affiliate Program
+                            </button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div className="relative min-h-full w-full">
+                <div className="relative z-10 mx-auto grid min-h-full max-w-6xl">
+                    <div className="container relative mx-auto my-auto text-center">
+                        <h3 className="text-blue-gray-900 block font-sans text-3xl font-bold leading-snug tracking-normal antialiased">
+                            Get started in 3 easy steps
+                        </h3>
+                        <div className="mt-20 flex flex-col items-center md:flex-row">
+                            <div className="w-full px-10 md:w-6/12 md:px-0 md:pr-20  md:text-right">
+                                <h6 className="text-blue-gray-900 block font-sans text-base font-semibold leading-relaxed tracking-normal antialiased">
+                                    Step 1
+                                </h6>
+                                <h3 className="text-blue-gray-900 mt-3 block font-sans text-3xl font-semibold leading-snug tracking-normal antialiased">
+                                    Create Account
+                                </h3>
+                                <p className="mt-3 block font-sans text-base font-light leading-relaxed text-gray-600 antialiased">
+                                    Sign up for our affiliate program to get
+                                    access to your unique tracking links and to
+                                    track your earnings. <br />
+                                    Get your{" "}
+                                    <a
+                                        href="https://affiliates.galichat.com/login"
+                                        target="_blank"
+                                        className="font-bold text-black underline"
+                                    >
+                                        affiliate link via Tolt
+                                    </a>
+                                    .
+                                </p>
+                                <img
+                                    src="https://www.galichat.com/tolt.png"
+                                    alt="Tolt"
+                                    className="mx-auto mt-5 w-36 md:ml-0 md:mr-auto"
+                                />
+                            </div>
+                            <div className="mt-10 flex w-full justify-end md:mt-0 md:w-6/12">
+                                <img
+                                    src="https://www.galichat.com/create-account.png"
+                                    alt="Step 1"
+                                    className="w-[600px]"
+                                />
+                            </div>
+                        </div>
+                        <div className="mt-20 flex flex-col-reverse items-center md:flex-row">
+                            <div className="mt-10 flex w-full justify-start md:mt-0 md:w-6/12">
+                                <img
+                                    src="https://www.galichat.com/make-refferals.png"
+                                    alt="Step 2"
+                                    className="w-[600px]"
+                                />
+                            </div>
+                            <div className="w-full px-10 md:w-6/12 md:px-0 md:pr-20 md:text-right">
+                                <h6 className="text-blue-gray-900 block font-sans text-base font-semibold leading-relaxed tracking-normal antialiased">
+                                    Step 2
+                                </h6>
+                                <h3 className="text-blue-gray-900 mt-3 block font-sans text-3xl font-semibold leading-snug tracking-normal antialiased">
+                                    Make Refferals
+                                </h3>
+                                <p className="mt-3 block font-sans text-base font-light leading-relaxed text-gray-600 antialiased">
+                                    Add your unique GaliChat links in your blog
+                                    posts, social media, or other promotional
+                                    content. This way, any traffic you send will
+                                    be registered and commisioned.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="mt-20 flex flex-col items-center md:flex-row">
+                            <div className="w-full px-10 md:w-6/12 md:px-0 md:pr-20 md:text-right">
+                                <h6 className="text-blue-gray-900 block font-sans text-base font-semibold leading-relaxed tracking-normal antialiased">
+                                    Step 3
+                                </h6>
+                                <h3 className="text-blue-gray-900 mt-3 block font-sans text-3xl font-semibold leading-snug tracking-normal antialiased">
+                                    Get Paid
+                                </h3>
+                                <p className="mt-3 block font-sans text-base font-light leading-relaxed text-gray-600 antialiased">
+                                    When a sale is made through a GaliChat
+                                    affiliate link, you will earn 30% of the
+                                    sale each month for up to 12 months, as long
+                                    as the subscription continues. You can check
+                                    all of your earnings in the dashboard.
+                                </p>
+                            </div>
+                            <div className="mt-10 flex w-full justify-end md:mt-0 md:w-6/12">
+                                <img
+                                    src="https://www.galichat.com/get-paid.png"
+                                    alt="Step 3"
+                                    className="w-[600px]"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <section className="px-8 py-20">
+                <div className="container mx-auto max-w-6xl">
+                    <div className="mb-8">
+                        <h3 className="text-blue-gray-900 mb-4 block font-sans text-4xl font-semibold !leading-snug tracking-normal antialiased">
+                            Frequently Asked Questions (FAQs)
+                        </h3>
+                        <p className="block font-sans text-base font-light leading-relaxed !text-gray-600 text-inherit antialiased lg:w-1/3">
+                            See what other people are asking. Don’t forget to
+                            try the chatbot available on the website for more
+                            information about our tool.
+                        </p>
+                    </div>
+                    <div className="mb-10 grid gap-8 md:grid-cols-1 lg:grid-cols-3">
+                        <div className="relative mb-6 flex flex-col rounded-xl bg-white bg-clip-border text-gray-700">
+                            <h4 className="text-blue-gray-900 block pb-6 font-sans text-2xl font-semibold leading-snug tracking-normal antialiased">
+                                What is Gali?
+                            </h4>
+                            <p className="block font-sans text-base font-normal leading-relaxed !text-gray-500 text-inherit antialiased">
+                                Gali is an advanced, smart chatbot designed with
+                                the latest technology to interact with users by
+                                understanding their questions and providing
+                                accurate answers. It can comprehend and respond
+                                to inquiries in real-time, 24 hours a day, seven
+                                days a week.
+                            </p>
+                        </div>
+                        <div className="relative mb-6 flex flex-col rounded-xl bg-white bg-clip-border text-gray-700">
+                            <h4 className="text-blue-gray-900 block pb-6 font-sans text-2xl font-semibold leading-snug tracking-normal antialiased">
+                                Am I able to become a partner?
+                            </h4>
+                            <p className="block font-sans text-base font-normal leading-relaxed !text-gray-500 text-inherit antialiased">
+                                We offer an affiliate partnership program open
+                                to any individual or business with clients or
+                                users who own websites and can integrate
+                                GaliChat.
+                            </p>
+                        </div>
+                        <div className="relative mb-6 flex flex-col rounded-xl bg-white bg-clip-border text-gray-700">
+                            <h4 className="text-blue-gray-900 block pb-6 font-sans text-2xl font-semibold leading-snug tracking-normal antialiased">
+                                How do I add GaliChat to a website?
+                            </h4>
+                            <p className="block font-sans text-base font-normal leading-relaxed !text-gray-500 text-inherit antialiased">
+                                To add Gali Chat to your website, you need to
+                                create an account, customize your chatbot, and
+                                then copy the code snippet provided to your
+                                website’s HTML. The chatbot will then be live on
+                                your website, ready to assist your customers.
+                            </p>
+                        </div>
+                        <div className="relative mb-6 flex flex-col rounded-xl bg-white bg-clip-border text-gray-700">
+                            <h4 className="text-blue-gray-900 block pb-6 font-sans text-2xl font-semibold leading-snug tracking-normal antialiased">
+                                How can I track my earnings as a Partner?
+                            </h4>
+                            <p className="block font-sans text-base font-normal leading-relaxed !text-gray-500 text-inherit antialiased">
+                                After you sign in to our affiliate program, you
+                                will get access to your own dashboard, where you
+                                can track your performance and earnings.
+                            </p>
+                        </div>
+                        <div className="relative mb-6 flex flex-col rounded-xl bg-white bg-clip-border text-gray-700">
+                            <h4 className="text-blue-gray-900 block pb-6 font-sans text-2xl font-semibold leading-snug tracking-normal antialiased">
+                                When do I get paid?
+                            </h4>
+                            <p className="block font-sans text-base font-normal leading-relaxed !text-gray-500 text-inherit antialiased">
+                                Payment will be tracked and sent to all ou
+                                affiliates every month.
+                            </p>
+                        </div>
+                        <div className="relative mb-6 flex flex-col rounded-xl bg-white bg-clip-border text-gray-700">
+                            <h4 className="text-blue-gray-900 block pb-6 font-sans text-2xl font-semibold leading-snug tracking-normal antialiased">
+                                How will you pay me?
+                            </h4>
+                            <p className="block font-sans text-base font-normal leading-relaxed !text-gray-500 text-inherit antialiased">
+                                We do the payments via PayPal, Wise, or bank
+                                transfer, based on your preference.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Affiliate
