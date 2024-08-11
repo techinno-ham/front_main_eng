1db74fdc365a8ea63a3db3fbb38fbe0522b07ea3
moein moeinnia <moein@pro.ai>
2024-08-11 16:09:39 +0330

    chore: text color changes

--- src/modules/landing/components/costSection/index.tsx
+++ src/modules/landing/components/costSection/index.tsx
@@ -29,38 +29,38 @@ const CostSection = () => {
            <div className="relative flex flex-col bg-clip-border rounded-xl bg-transparent text-gray-700 shadow-none items-center">
                           <DollarCircle
                             size="28"
-                            color="#212121"
+                            color="#3b82f6"
                            />
-                           <h1 className="block antialiased tracking-normal mb-2 mt-4 text-5xl text-gray-900 font-bold">50%-</h1>
-                           <h5 className="block antialiased tracking-normal text-xl leading-snug mb-2 font-semibold text-gray-600">هزینه های پشتیبانی مشتری</h5>
-                           <h6 className="block antialiased tracking-normal text-base leading-relaxed mb-2 font font-normal text-gray-600"> تا 50 درصد از هزینه های پشتیبانی مشتری خود را به ازای هر نماینده کاهش دهید.</h6>
+                           <h2 className="block antialiased tracking-normal mb-2 mt-4 text-5xl text-gray-900 font-bold">50%-</h2>
+                           <h5 className="block antialiased tracking-normal text-xl leading-snug mb-2 font-normal ">هزینه های پشتیبانی مشتری</h5>
+                           <h6 className="block antialiased tracking-normal text-base leading-relaxed mb-2 font font-normal text-zinc-400"> تا 50 درصد از هزینه های پشتیبانی مشتری خود را به ازای هر نماینده کاهش دهید.</h6>
                     </div>
                     <div className="relative flex flex-col bg-clip-border rounded-xl bg-transparent text-gray-700 shadow-none items-center">
                           <Timer1
                             size="28"
-                            color="#212121"
+                            color="#3b82f6"
                            />
-                           <h1 className="block antialiased tracking-normal mb-2 mt-4 text-5xl text-gray-900 font-bold">3x</h1>
-                           <h5 className="block antialiased tracking-normal text-xl leading-snug mb-2 font-semibold text-gray-600">پاسخ سریعتر</h5>
-                           <h6 className="block antialiased tracking-normal text-base leading-relaxed mb-2 font font-normal text-gray-600"> چت بات ما 3 برابر سریعتر از یک انسان پاسخ می دهد.</h6>
+                           <h2 className="block antialiased tracking-normal mb-2 mt-4 text-5xl text-gray-900 font-bold">3x</h2>
+                           <h5 className="block antialiased tracking-normal text-xl leading-snug mb-2 font-normal ">پاسخ سریعتر</h5>
+                           <h6 className="block antialiased tracking-normal text-base leading-relaxed mb-2 font font-normal text-zinc-400"> چت بات ما 3 برابر سریعتر از یک انسان پاسخ می دهد.</h6>
                     </div>
                     <div className="relative flex flex-col bg-clip-border rounded-xl bg-transparent text-gray-700 shadow-none items-center">
                           <DocumentText1
                             size="28"
-                            color="#212121"
+                            color="#3b82f6"
                            />
-                           <h1 className="block antialiased tracking-normal mb-2 mt-4 text-5xl text-gray-900 font-bold">100%</h1>
-                           <h5 className="block antialiased tracking-normal text-xl leading-snug mb-2 font-semibold text-gray-600">دقت</h5>
-                           <h6 className="block antialiased tracking-normal text-base leading-relaxed mb-2 font font-normal text-gray-600">چت بات ما در پاسخ به سوالات دقت 100% دارد.</h6>
+                           <h2 className="block antialiased tracking-normal mb-2 mt-4 text-5xl text-gray-900 font-bold">100%</h2>
+                           <h5 className="block antialiased tracking-normal text-xl leading-snug mb-2 font-normal ">دقت</h5>
+                           <h6 className="block antialiased tracking-normal text-base leading-relaxed mb-2 font font-normal text-zinc-400">چت بات ما در پاسخ به سوالات دقت 100% دارد.</h6>
                     </div>
                     <div className="relative flex flex-col bg-clip-border rounded-xl bg-transparent text-gray-700 shadow-none items-center">
                          <Messages3
                             size="28"
-                            color="#212121"
+                            color="#3b82f6"
                            />
-                           <h1 className="block antialiased tracking-normal mb-2 mt-4 text-5xl text-gray-900 font-bold">24/7</h1>
-                           <h5 className="block antialiased tracking-normal text-xl leading-snug mb-2 font-semibold text-gray-600">دستیار پشتیبانی</h5>
-                           <h6 className="block antialiased tracking-normal text-base leading-relaxed mb-2 font font-normal text-gray-600">در مقایسه با پشتیبانی استاندارد، چت بات 24/7 برای مشتریان شما در دسترس است.</h6>
+                           <h2 className="block antialiased tracking-normal mb-2 mt-4 text-5xl text-gray-900 font-bold">24/7</h2>
+                           <h5 className="block antialiased tracking-normal text-xl leading-snug mb-2 font-normal ">دستیار پشتیبانی</h5>
+                           <h6 className="block antialiased tracking-normal text-base leading-relaxed mb-2 font font-normal text-zinc-400">در مقایسه با پشتیبانی استاندارد، چت بات 24/7 برای مشتریان شما در دسترس است.</h6>
                     </div>
                     
                     
