cc747ce022c1ed174fb0e4242822e6112f9f9a35
mahdiyar <mahdiyarjfr@gmail.com>
2024-09-21 13:20:41 +0330

    feat: add go to my bot after register

--- src/modules/auth/hooks/register.ts
+++ src/modules/auth/hooks/register.ts
@@ -3,19 +3,36 @@ import { useState } from "react"
 import services from "../services"
 import { setCookie } from "nookies"
 import { toast } from "sonner"
+import useUserStore from "@/src/shared/store/userStore"
 
 const useRegister = () => {
     const [isLoading, setLoading] = useState(false)
     const [error, setError] = useState(null)
     const router = useRouter()
+    const { user, setUser, isAuthenticated, setIsAuthenticated } =
+    useUserStore()
 
     const registerAuth = async (user: any) => {
         setLoading(true)
         setError(null)
         try {
-            await services.register(user)
+        const res= await services.register(user)
+          setUser(res.data)
             toast.success("ثبت نام  شما موفق آمیز بود.")
-            router.push("/auth/login")
+            setCookie(null, "accessToken", res.data.accessToken, {
+                maxAge: 1 * 24 * 60 * 60,
+                path: "/",
+                secure: true, // Use this in production
+                sameSite: "strict", // Prevent CSRF attacks
+            })
+
+            setCookie(null, "refreshToken", res.data.refreshToken, {
+                maxAge: 3 * 24 * 60 * 60, // 3 days, or according to your refresh token TTL
+                path: "/",
+                secure: true, // Use this in production
+                sameSite: "strict",
+            })
+            router.push("/mybots")
         } catch (err: any) {
             if (err.message === "Email already registered") {
                 toast.error("ایمیل شما قبلا ثبت شده است")
