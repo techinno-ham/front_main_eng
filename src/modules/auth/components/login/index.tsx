750040a61ec13788bd4035fdc45bb3cf20e3929f
mahdiyar <mahdiyar@pro.ai>
2024-09-30 11:52:58 +0330

    feat: hard code for google url

--- src/modules/auth/components/login/index.tsx
+++ src/modules/auth/components/login/index.tsx
@@ -41,7 +41,7 @@ const Login = () => {
     const handleGoogleLogin = (event: any) => {
         event.preventDefault()
         console.log(process.env.NEXT_PUBLIC_BASE_API)
-        window.location.href = `${process.env.NEXT_PUBLIC_BASE_API}/auth/google`
+        window.location.href = `https://hamyar.chat/api/v1/auth/google`
     }
 
     return (
