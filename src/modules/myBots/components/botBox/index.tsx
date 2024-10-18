bb0d6e2d70457c25c600ed56dd60d72a4e441958
Moein Moeinnia <Moeinnia@protonmail.com>
2024-10-18 18:20:44 +0330

    chore: delete and copy functionality removed

--- src/modules/myBots/components/botBox/index.tsx
+++ src/modules/myBots/components/botBox/index.tsx
@@ -66,8 +66,8 @@ const BotBox = (props: IBotBoxProps) => {
     return (
         <>
             <div className="cursor-pointer rounded-xl border border-solid border-gray-200 hover:border-[#5470ff]">
-                <Link href={`/panel/${props.botsData.bot_id}`}>
-                    <div className="relative flex flex-col items-center">
+                <div className="relative flex flex-col items-center">
+                    <Link href={`/panel/${props.botsData.bot_id}`}>
                         <div className="absolute left-4 top-4">
                             <div className="flex h-9 w-9 items-center justify-center rounded-xl border bg-blue-600">
                                 <Global size="28" color="#Fff" />
@@ -81,14 +81,16 @@ const BotBox = (props: IBotBoxProps) => {
                                 src={"/double-wink.svg"}
                             />
                         </div>
-                        <div className="mt-3 w-full">
-                            <hr />
-                        </div>
-                        <div className="flex w-full items-center justify-between p-[9px]">
+                    </Link>
+                    <div className="mt-3 w-full">
+                        <hr />
+                    </div>
+                    <div className="flex w-full items-center justify-between p-[9px]">
+                        <Link href={`/panel/${props.botsData.bot_id}`}>
                             <div className="flex flex-col">
                                 <div>
                                     <span className="text-[20px]">
-                                        {props.botsData?.name || "هوشینو بات"}
+                                        {props.botsData?.name || "همیار بات"}
                                     </span>
                                 </div>
                                 <div>
@@ -105,82 +107,75 @@ const BotBox = (props: IBotBoxProps) => {
                                     </span>
                                 </div>
                             </div>
-
-                            <div className="flex w-1/4 justify-around">
-                                <div>
-                                    <button onClick={handleCopyBot}>
-                                        <DocumentCopy size={20} />
-                                    </button>
-                                </div>
-                                <div>
-                                    <button onClick={handleTrashClick}>
-                                        <Trash size={20} color="red" />
-                                    </button>
-                                    <Modal
-                                        open={open}
-                                        onClose={handleOnCloseModal}
-                                    >
-                                        <div className="w-80 text-center">
-                                            <div className="flex flex-col gap-2">
-                                                <div>
-                                                    <Trash
-                                                        size={50}
-                                                        className="mx-auto text-red-500"
-                                                    />
-                                                    <div className="mx-auto  w-60">
-                                                        <h3 className="text-lg font-black text-gray-800">
-                                                            حذف کردن چت بات
-                                                        </h3>
-                                                        <p className="mt-[8px] text-[12px] text-gray-500">
-                                                            آیا مطمئن هستید که
-                                                            می خواهید ربات چت
-                                                            خود را حذف کنید؟ این
-                                                            عمل قابل برگشت نیست.
-                                                        </p>
-                                                    </div>
-                                                </div>
-                                                <div className="flex justify-around gap-4">
-                                                    <button
-                                                        className="focus-visible:ring-ring inline-flex h-9 items-center justify-center whitespace-nowrap rounded-md border border-zinc-200 bg-transparent px-4 py-1 text-sm font-medium shadow-sm transition-colors hover:bg-zinc-100 hover:text-zinc-900 focus-visible:outline-none  focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-80"
-                                                        onClick={
-                                                            handleOnCloseModal
-                                                        }
-                                                    >
-                                                        لغو
-                                                    </button>
-                                                    <button
-                                                        style={{
-                                                            backgroundColor:
-                                                                "red",
-                                                        }}
-                                                        className="focus-visible:ring-ring inline-flex h-9 items-center justify-center whitespace-nowrap rounded-md  px-4 py-1 text-sm font-medium text-zinc-50 shadow-sm transition-colors hover:bg-red-500/90 focus-visible:outline-none    focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-80"
-                                                        type="button"
-                                                        onClick={handleDelete}
-                                                    >
-                                                        {loading ? (
-                                                            <>
-                                                                <div className="h-6 w-6 animate-spin rounded-full border-2 border-white border-t-blue-600"></div>
-                                                                <span className="mr-3">
-                                                                    صبر کنید ...
-                                                                </span>
-                                                            </>
-                                                        ) : (
-                                                            <>
-                                                                <span>
-                                                                    حذف کردن
-                                                                </span>
-                                                            </>
-                                                        )}
-                                                    </button>
+                        </Link>
+                        <div className="flex w-1/4 justify-around">
+                            <div>
+                                <button onClick={handleCopyBot}>
+                                    <DocumentCopy size={20} />
+                                </button>
+                            </div>
+                            <div>
+                                <button onClick={handleTrashClick}>
+                                    <Trash size={20} color="red" />
+                                </button>
+                                <Modal open={open} onClose={handleOnCloseModal}>
+                                    <div className="w-80 text-center">
+                                        <div className="flex flex-col gap-2">
+                                            <div>
+                                                <Trash
+                                                    size={50}
+                                                    className="mx-auto text-red-500"
+                                                />
+                                                <div className="mx-auto  w-60">
+                                                    <h3 className="text-lg font-black text-gray-800">
+                                                        حذف کردن چت بات
+                                                    </h3>
+                                                    <p className="mt-[8px] text-[12px] text-gray-500">
+                                                        آیا مطمئن هستید که می
+                                                        خواهید ربات چت خود را
+                                                        حذف کنید؟ این عمل قابل
+                                                        برگشت نیست.
+                                                    </p>
                                                 </div>
                                             </div>
+                                            <div className="flex justify-around gap-4">
+                                                <button
+                                                    className="focus-visible:ring-ring inline-flex h-9 items-center justify-center whitespace-nowrap rounded-md border border-zinc-200 bg-transparent px-4 py-1 text-sm font-medium shadow-sm transition-colors hover:bg-zinc-100 hover:text-zinc-900 focus-visible:outline-none  focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-80"
+                                                    onClick={handleOnCloseModal}
+                                                >
+                                                    لغو
+                                                </button>
+                                                <button
+                                                    style={{
+                                                        backgroundColor: "red",
+                                                    }}
+                                                    className="focus-visible:ring-ring inline-flex h-9 items-center justify-center whitespace-nowrap rounded-md  px-4 py-1 text-sm font-medium text-zinc-50 shadow-sm transition-colors hover:bg-red-500/90 focus-visible:outline-none    focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-80"
+                                                    type="button"
+                                                    onClick={handleDelete}
+                                                >
+                                                    {loading ? (
+                                                        <>
+                                                            <div className="h-6 w-6 animate-spin rounded-full border-2 border-white border-t-blue-600"></div>
+                                                            <span className="mr-3">
+                                                                صبر کنید ...
+                                                            </span>
+                                                        </>
+                                                    ) : (
+                                                        <>
+                                                            <span>
+                                                                حذف کردن
+                                                            </span>
+                                                        </>
+                                                    )}
+                                                </button>
+                                            </div>
                                         </div>
-                                    </Modal>
-                                </div>
+                                    </div>
+                                </Modal>
                             </div>
                         </div>
                     </div>
-                </Link>
+                </div>
             </div>
         </>
     )
