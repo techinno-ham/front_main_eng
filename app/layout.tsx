4d00e7a602411ae980fabbb0330cba8c59fa04ec
mahdiyar <mahdiyar@pro.ai>
2024-11-27 14:02:09 +0330

    feat:improce chat bot2

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
     children: React.ReactNode
 }>) {
     const pathname = usePathname()
-    const ChatBotScript =!pathname.startsWith("/chatbot");
+    const ChatBotScript =!pathname.startsWith("/test");
     const isMyBotsPath =
         pathname.startsWith("/mybots") ||
         pathname.startsWith("/panel") ||
