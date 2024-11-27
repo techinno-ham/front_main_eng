8bafe79f5964fb114e3e723090c0970c86621c4c
mahdiyar <mahdiyar@pro.ai>
2024-11-27 14:36:23 +0330

    feat:add new route4

--- app/chatbot/page.tsx
+++ app/chatbot/page.tsx
@@ -7,10 +7,12 @@ const ChatBotPage: NextPage = () => {
   const filePath = path.join(process.cwd(), "public", "chatbot", "index.html");
   const html = fs.readFileSync(filePath, "utf8");
 
+  const htmlFilePath = "/chatbot/index.html";
+
   return (
-    <div
-      dangerouslySetInnerHTML={{ __html: html }}
-      style={{ height: "100%", width: "100%" }}
+    <iframe
+      src={htmlFilePath}
+      style={{ height: "100vh", width: "100%", border: "none" }}
     />
   );
 };
